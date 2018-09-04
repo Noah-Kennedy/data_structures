a649cc7d69a397d7ac7bd381f94acfd30cfe198e
noah <nomaxx117@gmail.com>
2018-09-04 15:58:30 -0500

    Worked on linked lists

--- src/lists/linked_list.rs
+++ src/lists/linked_list.rs
@@ -1,16 +1,38 @@
-struct ListNode<'a, E> {
-    next: & 'a mut Option<ListNode<E>>,
+struct ListNode<'a, E: 'a> {
+    next: & 'a mut Option<ListNode<'a, E>>,
     value: E
 }
 
-struct LinkedList<'a, E> {
-    head: & 'a mut Option<ListNode<E>>,
-    size: u64
+struct LinkedList<'a, E: 'a> {
+    head: & 'a mut Option<ListNode<'a, E>>,
+    size: & 'a mut u64
 }
 
 impl<'a, E> super::List<'a, E> for LinkedList<'a, E> {
+
     fn insert(&mut self, element: E, index: u64) -> bool {
-        unimplemented!()
+
+        let result = true;
+
+        let mut iterator = self.head;
+
+        loop {
+            match iterator {
+                None => {
+                    let new_node = ListNode {
+                        next: & mut None,
+                        value: element
+                    };
+                    self.head = &mut Some(new_node);
+                },
+
+                Some(next_node) => {
+                    iterator = next_node.next;
+                }
+            }
+        }
+
+        true
     }
     
     fn get(&self, index: u64) -> Option<E> {
